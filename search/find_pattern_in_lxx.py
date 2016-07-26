ef970ea86a82e97ead6a84f0a4fe5298e88795cd
James Cuénod <j3frea@gmail.com>
2016-07-26 16:54:56 -0500

    Convert betacode output to unicode

--- search/find_pattern_in_lxx.py
+++ search/find_pattern_in_lxx.py
@@ -1,10 +1,29 @@
 from os import listdir
 from os.path import isfile, join
+import betacode as betacode
 import re
 import pprint
 
 mypath = "../lxxproject/files/"
 lxxfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+def get_unicode_word(wordInBetacode):
+	encoded_word = str.encode(betacode.transliterate(wordInBetacode))
+	return encoded_word.decode('utf-8')
+
+def convertArrayOfLinesFromBetaToUnicode(line_array):
+	ret = []
+	for line in line_array:
+		first_bit = re.search('^[^\s]+', line).group(0)
+		first_bit = get_unicode_word(first_bit).ljust(25)
+
+		last_bits = re.split("\s(?:[^\s|\n])", line[36:])
+
+		for i, b in enumerate(last_bits):
+			last_bits[i] = get_unicode_word(b).ljust(len(b))
+
+		ret.append(first_bit + line[25:36] + ''.join(last_bits).strip())
+	return ret
 
 sequences_to_match = {
 	# This search was for finding similar constructions to the title formula in Zech 1:1
@@ -36,7 +55,7 @@ for lxxfile in lxxfiles:
 				tmpMatch = []
 
 			if sequence_counter == len(sequence_match):
-				matches.append({ref: tmpMatch})
+				matches.append({ref: convertArrayOfLinesFromBetaToUnicode(tmpMatch)})
 				tmpMatch = []
 				sequence_counter = 0
 
